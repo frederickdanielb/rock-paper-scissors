3acb6fda7256c73d9461c3f4e624aadbfab51c28
Frederick Bustamante <fbustamante_externo@rayensalud.com>
2019-07-24 10:42:51 -0400

    clean code

--- front/src/utils/index.js
+++ front/src/utils/index.js
@@ -6,8 +6,5 @@ export default function getWinner(player1Choice, player2Choice, rules) {
 }
 
 export function getFinalWinner(score) {
-
-  if (player1Choice === player2Choice) return 'draws'
-  if (player2Choice === rules[0][player1Choice]) return 'wins';
-  else return 'losses';
+ //TODO: DETERMINA EL GANADOR QUE SE VERA EN ENDGAME
 }
